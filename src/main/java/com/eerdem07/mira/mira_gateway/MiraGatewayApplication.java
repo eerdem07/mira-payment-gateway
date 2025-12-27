02cef9ba00f90d0c21a5cfaa196e00d691907ca3
Emre Erdem <emreerdem502@gmail.com>
2025-12-28 00:46:22 +0300

    initial commit

---
+++ src/main/java/com/eerdem07/mira/mira_gateway/MiraGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.eerdem07.mira.mira_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiraGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiraGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eerdem07/mira/mira_gateway/MiraGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eerdem07.mira.mira_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiraGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
